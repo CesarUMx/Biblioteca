028a45ae7d24e315e21f391e6d5a21415f5a1215
aferretiz <aferretiz@mondragonmexico.edu.mx>
2024-06-04 14:30:32 -0600

    comit de inicio

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
-const base_url = "http://localhost/biblio/";
-const host = "localhost";
+const base_url = "http://localhost/Sistema-de-biblioteca-basico-php-8-y-mysql/";
+const host = "172.18.0.17";
 const user = "root";
-const pass = "";
+const pass = "password";
 const db = "biblioteca";
 const charset = "charset=utf8";
 ?>
